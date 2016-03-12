f0cd63df84c2237057b6a60a449f8c7d77dbe686
ggjack <gg.jack@hotmail.com>
2016-03-12 10:00:18 -0500

    Fixed bugs

--- background.js
+++ background.js
@@ -3,13 +3,13 @@ var state=false;
 function toggle(){
 	state=!state;
 	if(state){
-		chrome.browserAction.setIcon({path: "imgs/concentration_ON.png"});
+		chrome.browserAction.setIcon({path: "concentrate_ON.png"});
 	}
 	else{
-		chrome.browserAction.setIcon({path: "imgs/concentration_logo.png"});
+		chrome.browserAction.setIcon({path: "concentrate_logo.png"});
 
 	}
 }
 
 chrome.browserAction.onClicked.addListener(toggle);
-toggle();
+//toggle();
